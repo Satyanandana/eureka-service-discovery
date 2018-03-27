cf7a03988bdc61c8325f76f10265ca3d0a178b3f
Satyanandana <srikanthvarma.vadapalli@gmail.com>
2018-03-27 16:46:56 -0700

    update

--- src/main/java/com/demo/eurekaservicediscovery/EurekaServiceDiscoveryApplication.java
+++ src/main/java/com/demo/eurekaservicediscovery/EurekaServiceDiscoveryApplication.java
@@ -2,10 +2,11 @@ package com.demo.eurekaservicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceDiscoveryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceDiscoveryApplication.class, args);
 	}
